7d9fc6486621952f931e7787082a334922e7dbe1
xs5871 <xs5871@qn-4.net>
2024-11-30 14:20:18 +0000

    Add configurable range for ergobled gradient

--- includes/ergobled.h
+++ includes/ergobled.h
@@ -1,5 +1,5 @@
-void ergobled_mode_gradient_(void) {
-    float error = id(controller).get_error_value();
+void ergobled_mode_gradient_(const float range) {
+    float error = id(controller).get_error_value() / range;
     error = error < -1 ? -1 : (error > 1 ? 1 : error);
     if (error < 0) {
         id(ergobled).turn_on().set_rgb(-error, 1. + error, 0).perform();
